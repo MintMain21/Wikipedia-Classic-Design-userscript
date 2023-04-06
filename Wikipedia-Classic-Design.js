bf7f318e822563f995c73f1a6d7e5380692cdfcd
MintMain21 <91362684+MintMain21@users.noreply.github.com>
2023-04-06 12:36:03 -0700

    Created single "Skinchoice" parameter to change instead of two.

--- Wikipedia-Classic-Design.js
+++ Wikipedia-Classic-Design.js
@@ -18,12 +18,19 @@
 // @license      MIT
 // ==/UserScript==
 /**
+
 This userscript is based on https://github.com/MintMain21/Invidious-URL-Parameters-Userscript, which in turn is based on https://greasyfork.org/en/scripts/450983-genius-back-to-the-original-page-layout.
 It is designed to redirect wikipedia pages using the modern (2022) design to pages using a more classic design.
 By default, it redirects to a theme based on Wikipedia's appearence in 2008 (modern). Other themes are avalible based on designs from 2002 (cologneblue), 2010 (vector), etc. For more information, see https://en.wikipedia.org/wiki/Wikipedia:Skin
-To edit the enforced theme, change both (!url.includes("?useskin=[theme here]") and (window.location.replace(url + "?useskin=[theme here]").
-Anytime you load a wikipedia page, this script will check the URL for the desired parameters, and apply them if not found.
- */
+To edit the enforced theme, change the skinchoice below. */
+
+
+let skinchoice = 'modern';
+
+
+/**Anytime you load a wikipedia page, this script will check the URL for the desired parameters, and apply them if not found. */
+
+
 
 function getCurrentURL () {
   return window.location.href;
@@ -35,7 +42,7 @@ const url = getCurrentURL();
 (function() {
     'use strict';
 
-    if (!url.includes("?useskin=modern")) {
-        window.location.replace(url + "?useskin=modern");
+    if (!url.includes("?useskin=" + skinchoice)) {
+        window.location.replace(url + "?useskin=" + skinchoice);
     }
 })();
