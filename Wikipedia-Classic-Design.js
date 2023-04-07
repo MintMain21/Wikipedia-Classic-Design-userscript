71980cd396990e40171983a494154f442cdb5024
MintMain21 <91362684+MintMain21@users.noreply.github.com>
2023-04-06 17:26:16 -0700

    Update Wikipedia-Classic-Design.js

--- Wikipedia-Classic-Design.js
+++ Wikipedia-Classic-Design.js
@@ -19,7 +19,7 @@
 // ==/UserScript==
 /**
 
-This userscript is based on https://github.com/MintMain21/Invidious-URL-Parameters-Userscript, which in turn is based on https://greasyfork.org/en/scripts/450983-genius-back-to-the-original-page-layout.
+This userscript is based predominantly on https://greasyfork.org/en/scripts/458494-old-wikipedia-layout/code, as well as https://github.com/MintMain21/Invidious-URL-Parameters-Userscript.
 It is designed to redirect wikipedia pages using the modern (2022) design to pages using a more classic design.
 By default, it redirects to a theme based on Wikipedia's appearence in 2008 (modern). Other themes are avalible based on designs from 2002 (cologneblue), 2010 (vector), etc. For more information, see https://en.wikipedia.org/wiki/Wikipedia:Skin
 To edit the enforced theme, change the skinchoice below. */
